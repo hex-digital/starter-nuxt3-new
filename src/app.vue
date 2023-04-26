53d54cff242d1a79f10e6b86f50b937a21bc575f
Jamie Warburton <jamie@hexdigital.com>
2023-04-26 22:20:46 +0100

    fix: remove logger test

--- src/app.vue
+++ src/app.vue
@@ -5,10 +5,6 @@
 </template>
 
 <script setup lang="ts">
-import { logger } from '~/helpers/logger';
-
-logger.debug('test');
-
 useHead({
   title: 'Hex | Product and Growth Studio',
   link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
